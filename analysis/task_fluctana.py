4d830ed00adde127e7cbfdd1ed84f4be6f10b4fc
Ralph Kube <ralph_kube@gmx.net>
2019-11-18 13:49:51 -0800

    More work :)

--- analysis/task_fluctana.py
+++ analysis/task_fluctana.py
@@ -3,25 +3,23 @@
 import numpy as np
 
 # We need to add the projects directory to the pythonpath to accomodate dask
-import sys
-sys.path.append("/global/homes/r/rkube/repos/delta")
+#import sys
+#sys.path.append("/global/homes/r/rkube/repos/delta")
 #from diagnostics.ecei_channel_layout import ecei_channel_positions
 
 from analysis.channels import channel, channel_list
 from analysis.fluctana import FluctAna
 from analysis.kstarecei import KstarEcei
+from analysis import specs as sp
 
 
-# Defines rpos (in meters), zpos (in meters) and apos (in meters) for
-# the individual channels of the ECEI diagnostic
-#ecei_channel_positions = {"L2403": (4.3, 4.3, 4.3),
-#                          "L2406": (4.6, 4.6, 4.6)}
-
-
-def test_cross_power(Dlist, done=0, dtwo=1, done_subset=None, dtwo_subset=None):
+#def test_cross_power(Dlist, done=0, dtwo=1, done_subset=None, dtwo_subset=None):
+def test_cross_power(args):
     """Calculates cross-power for channels in a Dlist object
     Inputs:
     =======
+    Dlist: kstar_ecei object
+    channel_list: channel_list that labels the data in Dlist
 
 
     Outputs:
@@ -30,43 +28,51 @@ def test_cross_power(Dlist, done=0, dtwo=1, done_subset=None, dtwo_subset=None):
     # OUT : x-axis (ax), y-axis (val)
 
     """
+    Dlist, channel_list = args
 
-    #Dlist[dtwo].vkind = 'cross_power'
+    print("Dlist = ", Dlist)
+    print("Channel_list = ", channel_list)
 
-    if done_subset is not None: 
-        rnum = len(done_subset)
-    else:
-        rnum = len(Dlist[done].data)  # number of ref channels
-        done_subset = range(rnum)
 
-    if dtwo_subset is not None:
-        cnum = len(dtwo_subset)
-    else:
-        cnum = len(Dlist[dtwo].data)  # number of cmp channels
-        dtwo_subset = range(cnum)
+    for ch in channel_list:
+        print(ch.to_str())
 
-    # reference channel names
-    Dlist[dtwo].rname = []
+    # #Dlist[dtwo].vkind = 'cross_power'
 
-    # value dimension
-    Dlist[dtwo].val = np.zeros((cnum, len(Dlist[dtwo].ax)))
+    # if done_subset is not None: 
+    #     num_refs = len(done_subset)
+    # else:
+    #     rnum = len(Dlist[done].data)  # number of ref channels
+    #     done_subset = range(rnum)
 
-    # calculation loop for multi channels
-    for c in range(cnum):
-        # reference channel number
-        if rnum == 1:
-            Dlist[dtwo].rname.append(Dlist[done].clist[done_subset[0]])
-            XX = Dlist[done].spdata[done_subset[0],:,:]
-        else:
-            Dlist[dtwo].rname.append(Dlist[done].clist[done_subset[c]])
-            XX = Dlist[done].spdata[done_subset[c],:,:]
+    # if dtwo_subset is not None:
+    #     num_chs = len(dtwo_subset)
+    # else:
+    #     num_chs = len(Dlist[dtwo].data)  # number of cmp channels
+    #     dtwo_subset = range(cnum)
 
-        YY = Dlist[dtwo].spdata[dtwo_subset[c],:,:]
+    # # reference channel names
+    # Dlist[dtwo].rname = []
 
-        if Dlist[dtwo].ax[1] < 0: # full range
-            Dlist[dtwo].val[c,:] = sp.cross_power(XX, YY, Dlist[dtwo].win_factor)
-        else: # half
-            Dlist[dtwo].val[c,:] = 2*sp.cross_power(XX, YY, Dlist[dtwo].win_factor)  
+    # # value dimension
+    # Dlist[dtwo].val = np.zeros((cnum, len(Dlist[dtwo].ax)))
+
+    # # calculation loop for multi channels
+    # for c in range(cnum):
+    #     # reference channel number
+    #     if num_refs == 1:
+    #         Dlist[dtwo].rname.append(Dlist[done].clist[done_subset[0]])
+    #         XX = Dlist[done].spdata[done_subset[0],:,:]
+    #     else:
+    #         Dlist[dtwo].rname.append(Dlist[done].clist[done_subset[c]])
+    #         XX = Dlist[done].spdata[done_subset[c],:,:]
+
+    #     YY = Dlist[dtwo].spdata[dtwo_subset[c],:,:]
+
+    #     if Dlist[dtwo].ax[1] < 0: # full range
+    #         Dlist[dtwo].val[c,:] = sp.cross_power(XX, YY, Dlist[dtwo].win_factor)
+    #     else: # half
+    #         Dlist[dtwo].val[c,:] = 2*sp.cross_power(XX, YY, Dlist[dtwo].win_factor)  
 
 
 class task_fluctana():
@@ -88,7 +94,7 @@ class task_fluctana():
         self.fft_params = config["fft_params"]
 
         data_obj = KstarEcei(shotnr, [c.to_str() for c in self.ch_list], config)
-        print("Init...", data_obj.tt)
+
         self.fluctana = FluctAna()
         self.fluctana.Dlist.append(data_obj)
         # Flag that is set to False after first call to update_data
@@ -125,22 +131,21 @@ class task_fluctana():
     def method(self, dask_client):
         """Creates a wrapper to call the appropriate analysis function"""
         dummy_data = np.random.uniform(0.0, 1.0, 100)
-
-        #if self.analysis == "cross_power":
-            #fun = lambda A: A.cross_power
-        #return(dask_client.submit(test_analysis, dummy_data))
         return(dask_client.submit(print_path, dummy_data))
-        #return(None)
 
 
     def get_method(self):
-        #return np.mean
-        my_mean = test_cross_power
-        return my_mean
+        if self.analysis == "cross_power":
+            print("cross_power it is")
+            return test_cross_power
+
+        return None
 
     def get_data(self):
-        #return np.random.uniform(0.0, 1.0, 100)
-        return self.fluctana.Dlist[0].data
+        if self.analysis == "cross_power":
+            return (self.fluctana.Dlist[0], self.ch_list)
+
+        return None
 
 
 
