919eb33be74ad4629bfa5ffa4dd26b066b465dd5
Ralph Kube <ralph_kube@gmx.net>
2019-12-10 13:51:18 -0800

    Removed iteration over duplicate channels in iteration scheme

---
+++ analysis/ecei_helper.py
@@ -0,0 +1,233 @@
+#Encoding: UTF-8 -*-
+
+"""
+Contains helper function for working with the ECEI diagnostic
+"""
+
+import numpy as np
+
+def get_abcd(channel, LensFocus, LensZoom, Rinit, new_H=True):
+        """Input:
+        ch: channel
+        LensZoom: float
+        LensFocus: float
+        Rinit: ???
+        new_H: If true, use new values for H-dev, shot > 12957
+
+        Returns:
+        ========
+        ABCD: The ABCD matrix
+        """
+
+        # ABCD matrix
+        if channel.dev == 'L':
+            sp = 3350 - Rinit*1000  # [m] -> [mm]
+            abcd = np.array([[1,250+sp],[0,1]]).dot(
+                   np.array([[1,0],[(1.52-1)/(-730),1.52]])).dot(
+                   np.array([[1,135],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(2700*1.52),1/1.52]])).dot(
+                   np.array([[1,1265-LensZoom],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/1100,1.52]])).dot(
+                   np.array([[1,40],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(-1100*1.52),1/1.52]])).dot(
+                   np.array([[1,LensZoom],[0,1]])).dot(
+                   np.array([[1,0],[0,1.52]])).dot(
+                   np.array([[1,65],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(800*1.52),1/1.52]])).dot(
+                   np.array([[1,710-LensFocus+140],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/(-1270),1.52]])).dot(
+                   np.array([[1,90],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1270*1.52),1/1.52]])).dot(
+                   np.array([[1,539+35+LensFocus],[0,1]]))
+
+        elif channel.dev == 'H':
+            sp = 3350 - Rinit*1000
+            abcd = np.array([[1,250+sp],[0,1]]).dot(
+                   np.array([[1,0],[(1.52-1)/(-730),1.52]])).dot(
+                   np.array([[1,135],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(2700*1.52),1/1.52]])).dot(
+                   np.array([[1,1265-LensZoom],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/1100,1.52]])).dot(
+                   np.array([[1,40],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(-1100*1.52),1/1.52]])).dot(
+                   np.array([[1,LensZoom],[0,1]])).dot(
+                   np.array([[1,0],[0,1.52]])).dot(
+                   np.array([[1,65],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(800*1.52),1/1.52]]))
+
+            if new_H:
+                abcd = abcd.dot(
+                   np.array([[1,520-LensFocus+590-9.2],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/(-1100),1.52]])).dot(
+                   np.array([[1,88.4],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1100*1.52),1/1.52]])).dot(
+                   np.array([[1,446+35+LensFocus-9.2],[0,1]]))
+            else:
+                abcd = abcd.dot(
+                   np.array([[1,520-LensFocus+590],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/(-1400),1.52]])).dot(
+                   np.array([[1,70],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1400*1.52),1/1.52]])).dot(
+                   np.array([[1,446+35+LensFocus],[0,1]]))
+
+        elif channel.dev == 'G':
+            sp = 3150 - Rinit*1000
+            abcd = np.array([[1,1350-LensZoom+sp],[0,1]]).dot(
+                   np.array([[1,0],[0,1.545]])).dot(
+                   np.array([[1,100],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.545)/(900*1.545),1/1.545]])).dot(
+                   np.array([[1,1430-LensFocus+660+LensZoom+470],[0,1]])).dot(
+                   np.array([[1,0],[0,1.545]])).dot(
+                   np.array([[1,70],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.545)/(800*1.545),1/1.545]])).dot(
+                   np.array([[1,LensFocus-470],[0,1]])).dot(
+                   np.array([[1,0],[0,1.545]])).dot(
+                   np.array([[1,80],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.545)/(800*1.545),1/1.545]])).dot(
+                   np.array([[1,390],[0,1]]))
+
+        elif channel.dev == 'GT':
+            sp = 2300 - Rinit*1000
+            abcd = np.array([[1,sp+(1954-LensZoom)],[0,1]]).dot(
+                   np.array([[1,0],[(1.52-1)/(-1000),1.52]])).dot(
+                   np.array([[1,160],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1000*1.52),1/1.52]])).dot(
+                   np.array([[1,2280-(1954+160-LensZoom)],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/1000,1.52]])).dot(
+                   np.array([[1,20],[0,1]])).dot(
+                   np.array([[1,0],[0,1/1.52]])).dot(
+                   np.array([[1,4288-(2280+20)-LensFocus],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/(-1200),1.52]])).dot(
+                   np.array([[1,140],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1200*1.52),1/1.52]])).dot(
+                   np.array([[1,4520-(4288+140-LensFocus)],[0,1]])).dot(
+                   np.array([[1,0],[0,1.52]])).dot(
+                   np.array([[1,30],[0,1]])).dot(
+                   np.array([[1,0],[0,1/1.52]])).dot(
+                   np.array([[1,4940-(4520+30)],[0,1]]))
+
+        elif channel.dev == 'GR':
+            sp = 2300 - Rinit*1000
+            abcd = np.array([[1,sp+(1954-LensZoom)],[0,1]]).dot(
+                   np.array([[1,0],[(1.52-1)/(-1000),1.52]])).dot(
+                   np.array([[1,160],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1000*1.52),1/1.52]])).dot(
+                   np.array([[1,2280-(1954+160-LensZoom)],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/1000,1.52]])).dot(
+                   np.array([[1,20],[0,1]])).dot(
+                   np.array([[1,0],[0,1/1.52]])).dot(
+                   np.array([[1,4288-(2280+20)-LensFocus],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/(-1200),1.52]])).dot(
+                   np.array([[1,140],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1200*1.52),1/1.52]])).dot(
+                   np.array([[1,4520-(4288+140-LensFocus)],[0,1]])).dot(
+                   np.array([[1,0],[0,1.52]])).dot(
+                   np.array([[1,30],[0,1]])).dot(
+                   np.array([[1,0],[0,1/1.52]])).dot(
+                   np.array([[1,4940-(4520+30)],[0,1]]))
+
+        elif channel.dev == 'HT':
+            sp = 2300 - Rinit*1000
+            abcd = np.array([[1,sp+2586],[0,1]]).dot(
+                   np.array([[1,0],[0,1.52]])).dot(
+                   np.array([[1,140],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(770*1.52),1/1.52]])).dot(
+                   np.array([[1,4929-(2586+140)-LensZoom],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/(1200),1.52]])).dot(
+                   np.array([[1,20],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(-1200*1.52),1/1.52]])).dot(
+                   np.array([[1,5919-(4929+20-LensZoom)-LensFocus],[0,1]])).dot(
+                   np.array([[1,0],[(1.52-1)/(-1300),1.52]])).dot(
+                   np.array([[1,130],[0,1]])).dot(
+                   np.array([[1,0],[(1-1.52)/(1300*1.52),1/1.52]])).dot(
+                   np.array([[1,6489-(5919+130-LensFocus)],[0,1]])).dot(
+                   np.array([[1,0],[0,1.52]])).dot(
+                   np.array([[1,25.62],[0,1]])).dot(
+                   np.array([[1,0],[0,1/1.52]])).dot(
+                   np.array([[1,7094.62-(6489+25.62)],[0,1]]))
+
+        return abcd
+
+
+def beam_path(ch, LensFocus, LensZoom, rpos):
+    """Calculates the ray vertical position and angle at rpos [m] ray starting
+    from the array box position.
+
+    Input:
+    ======
+    ch, channel.
+    LensFocus, float
+    LensZoom: float
+    rpos: float, radial position of the channel view, in meters
+    ch_v: int, number of vertical channel
+    """
+
+    abcd = get_abcd(ch, LensFocus, LensZoom, rpos)
+
+    # vertical position from the reference axis (vertical center of all lens, z=0 line) at ECEI array box
+    zz = (np.arange(24, 0, -1) - 12.5) * 14  # [mm]
+    # angle against the reference axis at ECEI array box
+    aa = np.zeros(np.size(zz))
+
+    # vertical posistion and angle at rpos
+    za = np.dot(abcd, [zz, aa])
+    zpos = za[0][ch.ch_h - 1] * 1e-3  # zpos [m]
+    apos = za[1][ch.ch_h - 1]  # angle [rad] positive means the (z+) up-directed (divering from array to plasma)
+
+    return zpos, apos
+
+def channel_position(ch, ecei_cfg):
+    """Calculates the position of a channel in configuration space
+
+    Input:
+    ======1
+    ch, channel, The channel whos position we want to calculate
+    ecei_cfg, dict: Parameters of the ECEi diagnostic.
+    """
+    
+    me = 9.1e-31            # electron mass, in kg
+    e = 1.602e-19           # charge, in C
+    mu0 = 4 * np.pi * 1e-7  # permeability
+    ttn = 56*16             # total TF coil turns
+
+    # Unpack ecei_cfg
+    TFcurrent = ecei_cfg["TFcurrent"] # Instead of multiplying by 1e3, we put this in the config file
+    LoFreq = ecei_cfg["LoFreq"]
+    LensFocus = ecei_cfg["LensFocus"]
+    LensZoom = ecei_cfg["LensZoom"]
+    # Set hn, depending on mode. If mode is undefined, set X-mode as default.
+    try:
+        if ecei_cfg["Mode"] == 'O':
+            hn = 1
+        elif ecei_cfg["Mode"] == 'X':
+            hn = 2
+
+    except KeyError as k:
+        print("ecei_cfg: key {0:s} not found. Defaulting to 2nd X-mode".format(k.__str__()))
+        ecei_cfg["Mode"] = 'X'
+        hn = 2
+
+    rpos = hn * e * mu0 * ttn * TFcurrent /\
+                (4. * np.pi * np.pi * me * ((ch.ch_v - 1) * 0.9 + 2.6 + LoFreq) * 1e9)
+    zpos, apos = beam_path(ch, LensFocus, LensZoom, rpos)
+
+    return (rpos, zpos, apos)
+
+
+    #cnum = len(self.clist)
+    #self.rpos = np.zeros(cnum)  # R [m] of each channel
+    #self.zpos = np.zeros(cnum)  # z [m] of each channel
+    #self.apos = np.zeros(cnum)  # angle [rad] of each channel
+    #for c in range(0, cnum):
+    #    print(self.clist[c], self.cnidx1)
+    #    vn = int(self.clist[c][(self.cnidx1):(self.cnidx1+2)])
+    #    fn = int(self.clist[c][(self.cnidx1+2):(self.cnidx1+4)])
+
+    #    # assume cold resonance with Bt ~ 1/R
+    #    self.rpos[c] = self.hn*e*mu0*ttn*self.itf/((2*np.pi)**2*me*((fn - 1)*0.9 + 2.6 + self.lo)*1e9)
+
+    #    # get vertical position and angle at rpos
+    #    self.zpos[c], self.apos[c] = self.beam_path(self.rpos[c], vn)
+
+
+# End of file ecei_helper.py
---
+++ tests_analysis/test_kstar_ecei_helpers.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#-*- Encoding: UTF-8 -*-
+
+"""
+Do calculations with routines defined in kstar_ecei_helpers.py and verify against
+results from the original KstarEcei object.
+"""
+
+import sys
+import json
+
+sys.path.append("/global/homes/r/rkube/repos/delta")
+from analysis.channels import channel, channel_range
+from analysis.ecei_helper import get_abcd, beam_path, channel_position
+
+sys.path.append("/global/homes/r/rkube/repos/fluctana_rmc")
+from kstarecei import KstarEcei
+
+# Load a config file to get the ecei config
+with open("../configs/test_skw.json", "r") as df: 
+    cfg = json.load(df)
+ecei_cfg = cfg["ECEI_cfg"]
+# Define a test channel
+ch1 = channel('L', 17, 7)
+print("Channel ", ch1)
+
+# Calculate channel position using re-factored code
+res = channel_position(ch1, ecei_cfg)
+
+# Calculate channel positions using original code
+K = KstarEcei(shot=18431, clist=["ECEI_" + ch1.__str__()], data_path='/global/cscratch1/sd/rkube/KSTAR/kstar_streaming/')
+
+K.channel_position()
+print("KStar:")
+print("rpos = {0:6.4f}, zpos = {1:6.4f}, apos = {2:6.4f}".format(K.rpos[0], K.zpos[0], K.apos[0]))
+print("Re-factored:")
+print("rpos = {0:6.4f}, zpos = {1:6.4f}, apos = {2:6.4f}".format(*res))
+
+
+""" Some results.
+For Channel 1707 we have
+rpos = 1.2975, zpos = 0.0815, apos = 0.0036
+
+For Channel 0805 we have
+rpos = 1.3243, zpos = -0.0814, apos = -0.0036
+
+For Channel 1103 we have
+rpos = 1.3522, zpos = -0.0271, apos = -0.0012
+
+For Channel 0208 we have
+rpos = 1.2845, zpos = -0.1902, apos = -0.0083
+
+
+"""
+
+
+
+# End of file test_kstar_ecei_helpers.py
