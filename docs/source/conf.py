0c8b29630407c990cb779c023ea328099b811b2a
Ralph Kube <ralph.kube@uit.not>
2020-10-22 12:58:30 -0400

    Documentation building...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,11 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+# -- User configuration ------------------------------------------------------
+# Do not attempt to build mpi4py when building the documentation
+autodoc_mock_imports = ['mpi4py']
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
