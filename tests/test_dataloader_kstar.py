59ad553b75d6ac7501694f81639d5268405ef1a8
Ralph Kube <ralph.kube@uit.not>
2020-12-13 08:32:37 -0500

    Fixed dataloader tests

--- tests/test_dataloader_kstar.py
+++ tests/test_dataloader_kstar.py
@@ -11,11 +11,16 @@ TODO:
 
 """
 
-import mock
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+from tests.conftest import stream_attrs_018431
 import numpy as np
 
 
-def load_dummy_data(cls, array, idx_start, idx_end):
+def read_from_hdf5_dummy(cls, array, idx_start, idx_end):
     """Replaces _read_from_hdf5 in dataloader."""
     num_channels = array.shape[0]
 
@@ -23,7 +28,17 @@ def load_dummy_data(cls, array, idx_start, idx_end):
     array[:] = dummy_data[:]
 
 
-def test_dataloader_ecei_cached(config_all):
+def read_attributes_from_hdf5_dummy(cls, attrs):
+    """Sets dummy attributes.
+    
+    TODO: We use hard-coded stream_attrs_018431. This should really be the
+    fixture defined in conftest. But I don't know how to use the fixture in this
+    function and pass the argument in the mock.patch.multiple decorator.
+    """
+    cls.attrs = attrs
+
+
+def test_dataloader_ecei_cached(config_all, stream_attrs_018431):
     """Verify that _dataloader_ecei works correctly when using cached data."""
     import sys
     import os
@@ -31,10 +46,19 @@ def test_dataloader_ecei_cached(config_all):
     import numpy as np
     from delta.sources.dataloader import _loader_ecei
 
+    # Generate a read_attribute_dummy using the stream_attrs_... fixture.
+    # This needs to be a closure, as we can't pass the fixture as a function
+    # argument when we patch using the decorator call below:
+    def read_attrs_dummy(cls):
+        return read_attributes_from_hdf5_dummy(cls, stream_attrs_018431)
+
+
     cfg_all = config_all
     cfg_all["diagnostic"]["datasource"]["num_chunks"] = 5
     # Instantiate a loader where _read_from_hdf5 is replaced with load_dummy_data
-    with mock.patch.object(_loader_ecei, "_read_from_hdf5", new=load_dummy_data):
+    # with mock.patch.object(_loader_ecei, "_read_from_hdf5", new=read_from_hdf5_dummy):
+    with mock.patch.multiple(_loader_ecei, _read_from_hdf5=read_from_hdf5_dummy,
+                             _read_attributes_from_hdf5=read_attrs_dummy):
         my_loader = _loader_ecei(cfg_all, cache=True)
 
         for batch in my_loader.batch_generator():
@@ -42,7 +66,7 @@ def test_dataloader_ecei_cached(config_all):
             assert(np.abs(np.mean(batch.data) - 3.5) < 1e-2)
 
 
-def test_dataloader_ecei_nocache(config_all):
+def test_dataloader_ecei_nocache(config_all, stream_attrs_018431):
     """Verify that _dataloader_ecei works correctly when using cached data."""
     import sys
     import os
@@ -51,9 +75,18 @@ def test_dataloader_ecei_nocache(config_all):
     # # Import packages as delta.... so that we can run pytest as 
     from delta.sources.dataloader import _loader_ecei
 
+    # Generate a read_attribute_dummy using the stream_attrs_... fixture.
+    # This needs to be a closure, as we can't pass the fixture as a function
+    # argument when we patch using the decorator call below.
+    def read_attrs_dummy(cls):
+        return read_attributes_from_hdf5_dummy(cls, stream_attrs_018431)
+
     cfg_all = config_all
     # Instantiate a loader where _read_from_hdf5 is replaced with load_dummy_data
-    with mock.patch.object(_loader_ecei, "_read_from_hdf5", new=load_dummy_data):
+    # with mock.patch.object(_loader_ecei, "_read_from_hdf5", new=read_from_hdf5_dummy):
+    with mock.patch.multiple(_loader_ecei, _read_from_hdf5=read_from_hdf5_dummy,
+                             _read_attributes_from_hdf5=read_attrs_dummy):
+
         my_loader = _loader_ecei(cfg_all, cache=False)
 
         assert(my_loader.get_chunk_shape() == (192, cfg_all["diagnostic"]["datasource"]["chunk_size"]))
